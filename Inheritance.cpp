1526c73951af4bceb8cdf215e2474c5c34bb721d
Zoya-Hashmi <32487775+Zoya-Hashmi@users.noreply.github.com>
2020-04-16 22:02:50 +0500

    Create Inheritance.cpp

---
+++ Inheritance.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std
+
+class Sciences
+{
+  private:
+    int descriptor;
+  public:
+    void print()
+    {
+    cout<<descriptor;
+    }
+  
+}
+int main()
+{
+  return 0;
+}
